6aac08022c68bbc95908f7a4d957113219d02cb5
Tin <tinbuenafe@gmail.com>
2022-03-01 16:17:17 +0800

    Updates on mapping

--- 03_Making_Maps.R
+++ 03_Making_Maps.R
@@ -1,6 +1,6 @@
 # Loading packages and objects
 pacman::p_load("tidyverse", "sf", "raster", "terra", "rnaturalearth", "RColorBrewer", "cmocean", "magrittr", "patchwork")
-load(url("https://github.com/valentinitnelav/RandomScripts/blob/master/NaturalEarth.RData?raw=true"))
+load(url("https://github.com/valentinitnelav/RandomScripts/blob/master/NaturalEarth.RData?raw=true")) # to be used for plotting
 source("fSpatPlan_Convert2PacificRobinson.R")
 
 # Load species data
@@ -17,7 +17,7 @@ rob_pacific <- "+proj=robin +lon_0=180 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +
 land <- ne_countries(scale = 'large', returnclass = 'sf') %>% 
   fSpatPlan_Convert2PacificRobinson()
 
-# Plotting necessities
+# Plotting necessities (pulled from github.com/valentinitnelav and edited for use)
 NE_graticules_rob <- spTransform(NE_graticules, CRSobj = rob_pacific)
 NE_box_rob        <- spTransform(NE_box, CRSobj = cCRS)
 
@@ -30,27 +30,8 @@ lbl.X.prj <- cbind(prj.coord, lbl.X)
 names(lbl.X.prj)[1:2] <- c("X.prj","Y.prj")
 
 
-
-#### Creating barplots (total abundance per season) ####
-df <- spp %>% group_by(species, season) %>% 
-  summarize(total_abundance = sum(abundance))
-
-df$season <- factor(df$season, levels = c("jan-mar", "apr-jun", "jul-sept", "oct-dec"))
-
-StackedBar <- ggplot(df, aes(fill=species, y=total_abundance, x=season)) + 
-  geom_bar(position="stack", stat="identity") +
-  cmocean::scale_fill_cmocean(name = "deep",
-                              discrete = TRUE) +
-  theme_classic() +
-  theme(axis.text = element_text(size = 25),
-        axis.ticks.length = unit(.25, "cm"))
-ggsave("03_StackedBarplot.png",
-       plot = StackedBar, width = 21, height = 21, dpi = 300,
-       path = "Figures/")
-
-# To get top 8 most abundant species
-df %>% group_by(species) %>% summarize(total = sum(total_abundance)) %>% arrange(total) %>% print(n = Inf)
 #### Projecting data: Robinson Projection && Make Data Gridded 1x1 ####
+# Show all data as gridded data.
 make_GriddedData <- function(df, species_name, season_name) {
   longlat <- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
   rob_pacific <- "+proj=robin +lon_0=180 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
@@ -58,42 +39,98 @@ make_GriddedData <- function(df, species_name, season_name) {
   df <- df %>% 
     filter(species == species_name, season == season_name)
   
-  df_crs <- st_crs(longlat)
-  
-  df_sf <- st_as_sf(df, coords = c("longitude", "latitude"), crs = df_crs)
-  
-  df_poly <- df_sf %>% 
-    st_make_grid(cellsize = c(1,1), offset = st_bbox(df_sf)[c("xmin", "ymin")] - 0.5) %>% 
-    st_as_sf()
-  
-  idx <- st_contains(df_poly, df_sf, sparse = FALSE) %>%
-    rowSums() %>% 
-    as.logical()
-  
-  df_poly2 <- df_poly[idx,] %>% 
-    fSpatPlan_Convert2PacificRobinson() %>% 
-    as_tibble()
-  
-  df_tmp <- df %>% as_tibble() %>% 
-    cbind(., df_poly2) %>% 
-    st_as_sf(sf_column_name = "x") %>% 
-    dplyr::rename(geometry = x)
-  
+  if (dim(df)[1] != 0) {
+    df_crs <- st_crs(longlat)
+    
+    df_sf <- st_as_sf(df, coords = c("longitude", "latitude"), crs = df_crs)
+    
+    # Make grid around the limits of the area
+    df_poly <- df_sf %>% 
+      st_make_grid(cellsize = c(1,1), offset = st_bbox(df_sf)[c("xmin", "ymin")] - 0.5) %>% 
+      st_as_sf()
+    
+    # "Intersect" grids and points (from csv); TRUE if points are contained within the grid cells.
+    idx <- st_contains(df_poly, df_sf, sparse = FALSE) %>%
+      rowSums() %>% 
+      as.logical()
+    
+    # Project to Pacific-centered Robinson
+    df_poly2 <- df_poly[idx,] %>% 
+      fSpatPlan_Convert2PacificRobinson() %>% 
+      as_tibble()
+    
+    df_tmp <- df %>% as_tibble() %>% 
+      cbind(., df_poly2) %>% 
+      st_as_sf(sf_column_name = "x") %>% 
+      dplyr::rename(geometry = x)
+  }
+  else {
+    df_tmp <- NULL
+  }
+
   return(df_tmp)
 }
 make_SpeciesSeasonPlot <- function(df) {
   plot <- ggplot() + geom_sf(data = df, aes(fill = as.factor(abundance)), color = "grey64", size = 0.01) +
     geom_sf(data = land, fill = "grey20", color = NA, size = 0.01) +
     geom_polygon(data=NE_box_rob, aes(x=long, y=lat), colour="black", fill="transparent", size = 0.25) +
+    
     # add graticules projected to Robinson
     geom_path(data=NE_graticules_rob, aes(long, lat, group=group), linetype="dotted", color="grey50", size = 0.25) +
+    
     # add graticule labels – latitude and longitude
     geom_text(data = lbl.Y.prj, aes(x = X.prj, y = Y.prj, label = lbl), color="grey50", size=2) +
     geom_text(data = lbl.X.prj, aes(x = X.prj, y = Y.prj, label = lbl), color="grey50", size=2) +
     # the default, ratio = 1 in coord_fixed ensures that one unit on the x-axis is the same length as one unit on the y-axis
+    
     scale_fill_manual(values = c("#c7e9b4", "#7fcdbb", "#41b6c4", "#2c7fb8", "#253494"),
                         aesthetics = "fill",
                         name = "Abundance") + theme_classic() + theme(axis.title = element_blank())
+  
+  return(plot)
+}
+
+# Show 0s as points, and blend CPUE category 3 and 4 together. So, instead of having 5 categories, we have 3, just for plotting purposes.
+blend_SpeciesSeasonPlot <- function(df, species_name, season_name) {
+  
+  df_filtered <- df %>% 
+    dplyr::filter(species == species_name, season == season_name) %>% 
+    dplyr::filter(abundance == 0)
+  
+  df_crs <- st_crs(longlat)
+  
+  df_sf <- st_as_sf(df_filtered, coords = c("longitude", "latitude"), crs = df_crs) %>% 
+    fSpatPlan_Convert2PacificRobinson()
+  
+  df_tmp <- df %>% 
+    dplyr::filter(abundance > 0) %>% 
+    make_GriddedData(., species_name, season_name)
+  
+  plot <- ggplot()
+  if(!is_null(df_tmp)) {
+    df_tmp %<>% 
+      dplyr::mutate(category = case_when(abundance >= 3 ~ as.integer(3),
+                                        TRUE ~ abundance))
+    plot <- plot +
+      geom_sf(data = df_tmp, aes(fill = as.factor(category)), color = "grey64", size = 0.01)
+  }
+  
+  plot <- plot +
+    geom_sf(data = df_sf, size = 0.01, color = "grey64") +
+    geom_sf(data = land, fill = "grey20", color = NA, size = 0.01) +
+    geom_polygon(data=NE_box_rob, aes(x=long, y=lat), colour="black", fill="transparent", size = 0.25) +
+    
+    # add graticules projected to Robinson
+    geom_path(data=NE_graticules_rob, aes(long, lat, group=group), linetype="dotted", color="grey50", size = 0.25) +
+    
+    # add graticule labels – latitude and longitude
+    geom_text(data = lbl.Y.prj, aes(x = X.prj, y = Y.prj, label = lbl), color="grey50", size=2) +
+    geom_text(data = lbl.X.prj, aes(x = X.prj, y = Y.prj, label = lbl), color="grey50", size=2) +
+    # the default, ratio = 1 in coord_fixed ensures that one unit on the x-axis is the same length as one unit on the y-axis
+    
+    scale_fill_manual(values = c("#c7e9b4", "#41b6c4", "#253494"),
+                      aesthetics = "fill",
+                      name = "Abundance") + theme_classic() + theme(axis.title = element_blank())
   
   return(plot)
 }
@@ -111,52 +148,59 @@ tow_tmp <- tow %>%
   dplyr::mutate(longitude = longitude + 0.5, latitude = latitude + 0.5)
 
 #### Plotting Gridded Data ####
+# Saves the plots to Figures/ (make sure you have the folder/create the folder!)
 season_list <- c("jan-mar", "apr-jun", "jul-sept", "oct-dec")
-save_plots <- function(species_name) {
+save_plots <- function(species_name, blend = FALSE) { # Default: blend = FALSE; use blend = TRUE if you want to blend the plots (see above for explanation)
   for (i in 1:length(season_list)) {
-    spp_tmp %>% 
-      make_GriddedData(., species_name, season_list[i]) %>% 
-      make_SpeciesSeasonPlot(.) %>% 
-      ggsave(filename = paste0(species_name, "_", season_list[i], ".png"), path = "Figures/", width = 21, height = 21, dpi = 300)
+    if (isTRUE(blend)) {
+      blend_SpeciesSeasonPlot(df = spp_tmp, species_name, season_list[i]) %>% 
+        ggsave(filename = paste0(species_name, "_", season_list[i], "_Blended.png"), path = "Figures/", width = 21, height = 21, dpi = 300)
+    }
+    else {
+      spp_tmp %>% 
+        make_GriddedData(., species_name, season_list[i]) %>% 
+        make_SpeciesSeasonPlot(.) %>% 
+        ggsave(filename = paste0(species_name, "_", season_list[i], ".png"), path = "Figures/", width = 21, height = 21, dpi = 300)
+    }
+
   }
 }
-
 # Skipjack Tuna
-save_plots("skipjack-tuna")
+save_plots("skipjack-tuna", blend = TRUE)
 # Blue Marlin
-save_plots("blue-marlin")
+save_plots("blue-marlin", blend = TRUE)
 # Yellowfin Tuna
-save_plots("yellowfin-tuna")
+save_plots("yellowfin-tuna", blend = TRUE)
 # Albacore
-save_plots("albacore")
+save_plots("albacore", blend = TRUE)
 # Shortbill Spearfish
-save_plots("shortbill-spearfish")
+save_plots("shortbill-spearfish", blend = TRUE)
 # Frigate Tuna
-save_plots("frigate-tuna")
+save_plots("frigate-tuna", blend = TRUE)
 # Bigeye Tuna
-save_plots("bigeye-tuna")
+save_plots("bigeye-tuna", blend = TRUE)
 # Swordfish
-save_plots("swordfish")
+save_plots("swordfish", blend = TRUE)
 # Striped Marlin
-save_plots("striped-marlin")
+save_plots("striped-marlin", blend = TRUE)
 # Sauries
-save_plots("sauries")
+save_plots("sauries", blend = TRUE)
 # Sailfish
-save_plots("sailfish")
+save_plots("sailfish", blend = TRUE)
 # Longfin escolar
-save_plots("longfin-escolar")
+save_plots("longfin-escolar", blend = TRUE)
 # Bluefin tuna
-save_plots("bluefin-tuna")
+save_plots("bluefin-tuna", blend = TRUE)
 # Little tuna
-save_plots("little-tuna")
+save_plots("little-tuna", blend = TRUE)
 # Southern Bluefin Tuna
-save_plots("southern-bluefin-tuna")
+save_plots("southern-bluefin-tuna", blend = TRUE)
 # Slender tuna
-save_plots("slender-tuna")
+save_plots("slender-tuna", blend = TRUE)
 # Bonitos
-save_plots("bonitos")
+save_plots("bonitos", blend = TRUE)
 # Black marlin
-save_plots("black-marlin")
+save_plots("black-marlin", blend = TRUE)
 
 #### Map towing effort ####
 make_GriddedEffort <- function(df, effort_name, season_name) {
